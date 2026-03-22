3e3e07d57bda2a44b0bf3e86831b61af9bd8fd52
User <ulyanovskiy.2001@mail.ru>
2026-03-22 10:37:23 +0300

    feat

--- src/converters/utils.ts
+++ src/converters/utils.ts
@@ -1,9 +1,11 @@
 import { JsonCodes } from "../utils/constants"
 
-const whitespace = new Set([JsonCodes.SPACE, JsonCodes.TAB, JsonCodes.NEW_LINE, JsonCodes.CARRIAGE_RETURN])
+const isWhitespace = (byte: number) =>
+    byte === JsonCodes.SPACE || byte === JsonCodes.NEW_LINE ||
+    byte === JsonCodes.TAB || byte === JsonCodes.CARRIAGE_RETURN
 
 export function skipWhitespace(bytes: Uint8Array<ArrayBuffer>, i: number): number {
-    while (i < bytes.length && whitespace.has(bytes[i]))
+    while (i < bytes.length && isWhitespace(bytes[i]))
         i++
     return i
 }
