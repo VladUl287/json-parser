e45433656d1088e3ad56afe96191348e1343879a
User <ulyanovskiy.2001@mail.ru>
2026-03-24 10:13:56 +0300

    feat: optimize whitespace skip

--- src/converters/utils.ts
+++ src/converters/utils.ts
@@ -1,8 +1,12 @@
 import { JsonCodes } from "../utils/constants"
 
-const isWhitespace = (byte: number) =>
-    byte === JsonCodes.SPACE || byte === JsonCodes.NEW_LINE ||
-    byte === JsonCodes.TAB || byte === JsonCodes.CARRIAGE_RETURN
+const isWhitespaceBitMap = new Uint8Array(256)
+isWhitespaceBitMap[JsonCodes.SPACE] = 1
+isWhitespaceBitMap[JsonCodes.NEW_LINE] = 1
+isWhitespaceBitMap[JsonCodes.TAB] = 1
+isWhitespaceBitMap[JsonCodes.CARRIAGE_RETURN] = 1
+
+export const isWhitespace = (byte: number) => isWhitespaceBitMap[byte]
 
 export function skipWhitespace(bytes: Uint8Array<ArrayBuffer>, i: number): number {
     while (i < bytes.length && isWhitespace(bytes[i]))
