ebd739b03cc4713554d48a47cd9792fb409e826b
User <ulyanovskiy.2001@mail.ru>
2026-03-25 11:01:18 +0300

    tests: update simd loop benchmark

--- benchmarks/simdloop.bench.ts
+++ benchmarks/simdloop.bench.ts
@@ -11,91 +11,90 @@ const stringBig = new TextEncoder().encode("phoneNumberphoneNumberphoneNumberpho
 const bigBytes = new Uint8Array([...stringBig])
 
 suite
-    .add('equals_small', () => equals(smallBytes, stringSmall))
-    .add('simd_small_8', () => equals_simd_8(smallBytes, stringSmall, 0))
-    .add('simd_small', () => equals_simd_4(smallBytes, stringSmall, 0))
-    .add('loop_small', () => equals_loop(smallBytes, stringSmall, 0))
-    .add('equals_default', () => equals(defaultBytes, defaultBytes))
-    .add('simd_default_8', () => equals_simd_8(defaultBytes, stringDefault, 0))
-    .add('simd_default', () => equals_simd_4(defaultBytes, stringDefault, 0))
-    .add('loop_default', () => equals_loop(defaultBytes, stringDefault, 0))
-    .add('equals_big', () => equals(bigBytes, bigBytes))
-    .add('simd_big_8', () => equals_simd_8(bigBytes, stringBig, 0))
-    .add('simd_big', () => equals_simd_4(bigBytes, stringBig, 0))
-    .add('loop_big', () => equals_loop(bigBytes, stringBig, 0))
-    .on('cycle', function (event) { console.log(String(event.target)) })
-    .on('complete', function () { formatBenchmarkResults(this) })
+    .add('equals_small', () => equals(smallBytes, stringSmall, 0, 0))
+    .add('simd_small_8', () => equals_simd_8(smallBytes, stringSmall, 0, 0))
+    .add('simd_small', () => equals_simd_4(smallBytes, stringSmall, 0, 0))
+    .add('loop_small', () => equals_loop(smallBytes, stringSmall, 0, 0))
+    .add('equals_default', () => equals(defaultBytes, defaultBytes, 0, 0))
+    .add('simd_default_8', () => equals_simd_8(defaultBytes, stringDefault, 0, 0))
+    .add('simd_default', () => equals_simd_4(defaultBytes, stringDefault, 0, 0))
+    .add('loop_default', () => equals_loop(defaultBytes, stringDefault, 0, 0))
+    .add('equals_big', () => equals(bigBytes, bigBytes, 0, 0))
+    .add('simd_big_8', () => equals_simd_8(bigBytes, stringBig, 0, 0))
+    .add('simd_big', () => equals_simd_4(bigBytes, stringBig, 0, 0))
+    .add('loop_big', () => equals_loop(bigBytes, stringBig, 0, 0))
+    .on('cycle', function (event) {
+        console.log(String(event.target))
+    }
+    )
+    .on('complete', function () {
+        formatBenchmarkResults(this)
+    })
     .run({ 'async': true })
 
-function equals(a: Uint8Array, b: Uint8Array): boolean {
+function equals(a: Uint8Array, b: Uint8Array, aI: number, bI: number): boolean {
     if (a.length !== b.length) return false
-    if (a.length < 4) return equals_loop(a, b, 0)
-    if (a.length < 8) return equals_simd_4(a, b, 0)
-    return equals_simd_8(a, b, 0)
+    if (a.length < 4) return equals_loop(a, b, aI, bI)
+    if (a.length < 8) return equals_simd_4(a, b, aI, bI)
+    return equals_simd_8(a, b, aI, bI)
 }
 
-function equals_loop(a: Uint8Array, b: Uint8Array, start: number): boolean {
-    let j = start
-    let index = start
-    while (j < b.length) {
-        if (a[index] !== b[j])
+function equals_loop(a: Uint8Array, b: Uint8Array, aI: number, bI: number): boolean {
+    while (aI < b.length) {
+        if (a[aI] != b[bI])
             return false
-        index++
-        j++
+        aI++
+        bI++
     }
     return true
 }
 
-function equals_simd_4(aArr: Uint8Array, bArr: Uint8Array, start: number) {
-    let j = start
-    let index = start
-    while (j < bArr.length - 4) {
-        const a = aArr[index]
-        const b = aArr[index + 1]
-        const c = aArr[index + 2]
-        const d = aArr[index + 3]
+function equals_simd_4(aArr: Uint8Array, bArr: Uint8Array, aI: number, bI: number) {
+    while (aI < bArr.length - 4) {
+        const a = aArr[aI]
+        const b = aArr[aI + 1]
+        const c = aArr[aI + 2]
+        const d = aArr[aI + 3]
 
-        const aa = bArr[j]
-        const bb = bArr[j + 1]
-        const cc = bArr[j + 2]
-        const dd = bArr[j + 3]
+        const aa = bArr[bI]
+        const bb = bArr[bI + 1]
+        const cc = bArr[bI + 2]
+        const dd = bArr[bI + 3]
 
         if (a != aa || b != bb || c != cc || d != dd)
             return false
 
-        index += 4
-        j += 4
+        aI += 4
+        bI += 4
     }
-    return equals_loop(aArr, bArr, j)
+    return equals_loop(aArr, bArr, aI, bI)
 }
 
-function equals_simd_8(aArr: Uint8Array, bArr: Uint8Array, start: number): boolean {
-    let j = start
-    let index = start
-    while (j < bArr.length - 8) {
-        const a = aArr[index]
-        const b = aArr[index + 1]
-        const c = aArr[index + 2]
-        const d = aArr[index + 3]
-        const e = aArr[index + 4]
-        const f = aArr[index + 5]
-        const g = aArr[index + 6]
-        const x = aArr[index + 7]
+function equals_simd_8(aArr: Uint8Array, bArr: Uint8Array, aI: number, bI: number): boolean {
+    while (aI < bArr.length - 8) {
+        const a = aArr[aI]
+        const b = aArr[aI + 1]
+        const c = aArr[aI + 2]
+        const d = aArr[aI + 3]
+        const e = aArr[aI + 4]
+        const f = aArr[aI + 5]
+        const g = aArr[aI + 6]
+        const x = aArr[aI + 7]
 
-        const aa = bArr[j]
-        const bb = bArr[j + 1]
-        const cc = bArr[j + 2]
-        const dd = bArr[j + 3]
-        const ee = bArr[j + 4]
-        const ff = bArr[j + 5]
-        const gg = bArr[j + 6]
-        const xx = bArr[j + 7]
+        const aa = bArr[bI]
+        const bb = bArr[bI + 1]
+        const cc = bArr[bI + 2]
+        const dd = bArr[bI + 3]
+        const ee = bArr[bI + 4]
+        const ff = bArr[bI + 5]
+        const gg = bArr[bI + 6]
+        const xx = bArr[bI + 7]
 
         if (a != aa || b != bb || c != cc || d != dd || e != ee || f != ff || g != gg || x != xx)
             return false
 
-        index += 8
-        j += 8
+        aI += 8
+        bI += 8
     }
-    return equals_simd_4(aArr, bArr, j)
+    return equals_simd_4(aArr, bArr, aI, bI)
 }
