39d132cecbed5b6101ff17ab958ba75c0aabbe28
User <ulyanovskiy.2001@mail.ru>
2026-03-26 11:26:24 +0300

    test: udpate behc lib for deserializer on tinybench

--- benchmarks/deserializer.ts
+++ benchmarks/deserializer.ts
@@ -1,13 +1,12 @@
-import Benchmark from 'benchmark'
-import { formatBenchmarkResults } from './utils'
+import { Bench } from 'tinybench'
 import { toMetadata } from '../src/metadata/metadata'
 import { deserialize } from '../src/json'
 
-const suite = new Benchmark.Suite()
+const suite = new Bench({ name: 'deserialization', warmupIterations: 200 })
 
 const obj = {
-    id: 15,
-    order: 1244,
+    id: 343543534,
+    order: 343543534,
     phone: 343543534,
     phone1: 343543534,
     phone2: 343543534,
@@ -21,10 +20,7 @@ const obj = {
     phone10: 343543534,
     phone11: 343543534,
     phone12: 343543534,
-    phone13: 343543534,
-    // phone: {
-    //     code: 7
-    // },
+    phone13: 343543534
 }
 
 const value = JSON.stringify(obj, null, 4)
@@ -32,15 +28,10 @@ const valueBytes = new TextEncoder().encode(value)
 const metadata = toMetadata(obj)
 
 suite
-    .add('custom', () => deserialize(valueBytes, metadata))
-    .add('native', () => JSON.parse(value))
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function () {
-        console.log('Fastest is ' + this.filter('fastest').map('name'));
-    })
-    .on('complete', function () {
-        formatBenchmarkResults(this)
-    })
-    .run({ 'async': true })
+    .add('deserialize', () => deserialize(valueBytes, metadata))
+    .add('JSON.parse', () => JSON.parse(value))
+
+suite.run().then(() => {
+    console.log(suite.name)
+    console.table(suite.table())
+})
