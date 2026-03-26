e232f920bddc45cc577609f152b4d79059b4b731
User <ulyanovskiy.2001@mail.ru>
2026-03-26 11:38:11 +0300

    feat: update array equality

--- benchmarks/array_equal.ts
+++ benchmarks/array_equal.ts
@@ -1,8 +1,7 @@
-import Benchmark from 'benchmark'
-import { formatBenchmarkResults } from './utils'
-import { equals, equals_bitwise, equals_loop } from '../src/utils/array'
+import { Bench } from 'tinybench'
+import { equals, equals_loop } from '../src/utils/array'
 
-const suite = new Benchmark.Suite()
+const suite = new Bench({ name: 'array_equal', warmupIterations: 200 })
 
 const stringSmall = new TextEncoder().encode("phone")
 const smallBytes = new Uint8Array([...stringSmall])
@@ -13,19 +12,13 @@ const bigBytes = new Uint8Array([...stringBig])
 
 suite
     .add('equals_small', () => equals(smallBytes, stringSmall, 0, 0))
-    .add('equals_bitwise', () => equals_bitwise(smallBytes, stringSmall, 0, 0))
     .add('loop_small', () => equals_loop(smallBytes, stringSmall, 0, 0))
     .add('equals_default', () => equals(defaultBytes, stringDefault, 0, 0))
-    .add('equals_bitwise', () => equals_bitwise(defaultBytes, stringDefault, 0, 0))
     .add('loop_default', () => equals_loop(defaultBytes, stringDefault, 0, 0))
-    .add('equals_big', () => equals(bigBytes, bigBytes, 0, 0))
-    .add('equals_bitwise', () => equals_bitwise(bigBytes, stringBig, 0, 0))
+    .add('equals_big', () => equals(bigBytes, stringBig, 0, 0))
     .add('loop_big', () => equals_loop(bigBytes, stringBig, 0, 0))
-    .on('cycle', function (event) {
-        console.log(String(event.target))
-    }
-    )
-    .on('complete', function () {
-        formatBenchmarkResults(this)
-    })
-    .run({ 'async': true })
+
+suite.run().then(() => {
+    console.log(suite.name)
+    console.table(suite.table())
+})
